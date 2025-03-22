ea11edd09dccf542de5fb9abc249d2b727dde64c
Thinkpad <thinkpaduser0531@gmail.com>
2025-03-22 18:56:00 -0500

    Changed index page title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Quotes REST API</title>
 </head>
 <body>
     <h1>Quotes REST API</h1>
